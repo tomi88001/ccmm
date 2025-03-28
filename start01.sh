afbaae913e8d7a546fceaaa802833cbb009a82d4
tm <tm886001@gmail.com>
2025-03-29 00:33:33 +0100

    1

--- start01.sh
+++ start01.sh
@@ -21,7 +21,7 @@ pip3 install requests pysocks
 # 运行 Python 脚本，并后台执行
 PYTHON_SCRIPT="atk201.py"
 sh stop.sh
-if [ "$1" == "back" ]; then
+if [ "$1" = "back" ]; then
     if [ ! -d "$PYTHON_SCRIPT" ]; then
         nohup python3 "$PYTHON_SCRIPT" > output.log 2>&1 &
         tail -f output.log
@@ -29,7 +29,5 @@ if [ "$1" == "back" ]; then
 else
     python3 "$PYTHON_SCRIPT"
 fi
-
-
-
 echo "脚本已启动，日志保存在 output.log"
+ps aux | grep 'python3 atk2' | grep -v grep
